8027198efa69c19f7e4862a177ed5f116d78931b
woody <paulsherwood.basongit@gmail.com>
2022-05-19 06:16:45 +0000

    Initial commit.

--- lamp.sh
+++ lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Purpose: Install LAMP STACK
-#
+# Env: CentOS
 
 #parse ip address and print on the screen.
 IP_ADDR=$(ip addr show eth0 | grep inet | awk '{print $2}')
@@ -9,8 +9,32 @@ echo "IP Address: $IP_ADDR"
 
 
 install_package () {
-	echo "Package Function"
+	yum install update > update.txt
+
+	#testing logic kkk.
+	if cat update.txt | grep "No package" > /dev/null; then
+		echo "Updated."
+	fi
+
+	#httpd/apache installation.
+	echo "### Installing HTTPD ###"
+	sudo yum install httpd -y > /dev/null
+	echo "httpd service installed."
+	
+	#enable httpd during restart.
+	sudo systemctl enable httpd
+
+	#start httpd.
+	sudo systemctl start httpd
+	
+	#check status of httpd service.
+	sudo systemctl status httpd 
+
+	echo
+
+	curl -X GET localhost > index.html
 }
+
 
 #check if user is a root user.
 if [[ "${UID}" -ne 0 ]]; then
@@ -19,6 +43,7 @@ if [[ "${UID}" -ne 0 ]]; then
 	break
 else
 	echo "root user"
+	echo
 	install_package
 fi
 
