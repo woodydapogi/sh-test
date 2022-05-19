c8c82eb50e36ddc92a323d04e8fe7d1d6d212577
woody <paulsherwood.basongit@gmail.com>
2022-05-19 06:03:51 +0000

    Initial commit

--- lamp.sh
+++ lamp.sh
@@ -2,16 +2,24 @@
 # Purpose: Install LAMP STACK
 #
 
+#parse ip address and print on the screen.
+IP_ADDR=$(ip addr show eth0 | grep inet | awk '{print $2}')
+
+echo "IP Address: $IP_ADDR"
+
+
+install_package () {
+	echo "Package Function"
+}
+
+#check if user is a root user.
 if [[ "${UID}" -ne 0 ]]; then
 	echo "user need to be root."
 	echo "sudo -i | su -"
+	break
 else
 	echo "root user"
+	install_package
 fi
 
-main() {
-	echo "Ok."
-}
-
-main
-
+exit 0
